3d671eba9ad22d8b1f0aa3e98473908932cd8aa3
Rémy HUBSCHER <hubscher.remy@gmail.com>
2012-10-24 18:43:25 +0200

    #8 return the length of the queue; #2 Call Redis in ASYNC mode; #7 GET / without params display the name and the version

--- insight_reloaded/api.py
+++ insight_reloaded/api.py
@@ -1,10 +1,11 @@
+import json
+import os
+
 import tornadoredis
 import tornado.web
 import tornado.ioloop
 import tornado.gen
 import tornado.httpserver
-
-import json
 
 try:
     import settings
@@ -16,11 +17,31 @@ REDIS_PORT = getattr(settings, 'REDIS_PORT', 6379)
 REDIS_DB = getattr(settings, 'REDIS_PORT', 0)
 REDIS_QUEUE_KEY = getattr(settings, 'REDIS_QUEUE_KEY', 'insight_reloaded')
 
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(HERE, '../VERSION')) as f:
+        VERSION=f.readlines()[0].strip()
+except IOError:
+    VERSION='N/A'
+
+
 c = tornadoredis.Client(host=REDIS_HOST, port=REDIS_PORT, selected_db=REDIS_DB)
 c.connect()
 
 class MainHandler(tornado.web.RequestHandler):
+    """Convert the querystring in a REDIS job JSON."""
+    @tornado.web.asynchronous
     def get(self):
+        # If there is no argument, display the version
+        if self.request.arguments == {}:
+            self.write(dict(insight_reloaded="Bonjour", 
+                            name="insight-reloaded", 
+                            version=VERSION))
+            self.finish()
+            return
+
+        # Else process the request
         params = {'url': self.get_argument("url", None),
                   'callback': self.get_argument("callback", None),}
         # Get URL
@@ -39,13 +60,27 @@ class MainHandler(tornado.web.RequestHandler):
         params['crop'] = self.get_argument('crop', False) and True
 
         message = json.dumps(params)
-        c.rpush(REDIS_QUEUE_KEY, message)
-    
-        self.write("Job added to queue.")
+        c.rpush(REDIS_QUEUE_KEY, message, self.on_response)
+
+    def on_response(self, response):
+        self.write(dict(insight_reloaded="Job added to queue.",
+                        number_in_queue=response))
+        self.finish()
+
+class StatusHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        c.llen(REDIS_QUEUE_KEY, self.on_response)
+
+    def on_response(self, response):
+        self.write(dict(insight_reloaded="There is %d job in the queue." % response,
+                        number_in_queue=response))
+        self.finish()
 
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/status", StatusHandler),
 ])
 
 def main():
